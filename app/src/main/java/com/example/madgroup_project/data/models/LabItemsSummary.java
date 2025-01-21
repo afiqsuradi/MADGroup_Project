61d310583317c1e436753fa8098c6cf1ad12f8d9
Aiman Afiq <aimanafiq05@gmail.com>
2025-01-21 23:37:33 +0800

    fixed sql compilation error

--- app/src/main/java/com/example/madgroup_project/data/models/LabItemsSummary.java
+++ app/src/main/java/com/example/madgroup_project/data/models/LabItemsSummary.java
@@ -1,31 +1,41 @@
 package com.example.madgroup_project.data.models;
 
+import androidx.room.ColumnInfo;
+
 public class LabItemsSummary {
-        private int totalItems;
-        private int workingItems;
-        private int maintenanceItems;
-        private int brokenItems;
 
-        public LabItemsSummary(int totalItems, int workingItems, int maintenanceItems, int brokenItems) {
-            this.totalItems = totalItems;
-            this.workingItems = workingItems;
-            this.maintenanceItems = maintenanceItems;
-            this.brokenItems = brokenItems;
-        }
+    @ColumnInfo(name = "total_items")
+    private int totalItems;
 
-        public int getTotalItems() {
-            return totalItems;
-        }
+    @ColumnInfo(name = "working_items")
+    private int workingItems;
 
-        public int getWorkingItems() {
-            return workingItems;
-        }
+    @ColumnInfo(name = "maintenance_items")
+    private int maintenanceItems;
 
-        public int getMaintenanceItems() {
-            return maintenanceItems;
-        }
+    @ColumnInfo(name = "broken_items")
+    private int brokenItems;
 
-        public int getBrokenItems() {
-            return brokenItems;
-        }
+    public LabItemsSummary(int totalItems, int workingItems, int maintenanceItems, int brokenItems) {
+        this.totalItems = totalItems;
+        this.workingItems = workingItems;
+        this.maintenanceItems = maintenanceItems;
+        this.brokenItems = brokenItems;
+    }
+
+    public int getTotalItems() {
+        return totalItems;
+    }
+
+    public int getWorkingItems() {
+        return workingItems;
+    }
+
+    public int getMaintenanceItems() {
+        return maintenanceItems;
+    }
+
+    public int getBrokenItems() {
+        return brokenItems;
+    }
 }
